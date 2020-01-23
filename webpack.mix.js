b1f092ee860e1e298ae9e35a5b909965d9ab2fa8
Ringo De Smet <ringo@de-smet.name>
2020-01-23 22:56:17 +0100

    Build the bundle zip via Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,42 @@ const mix = require('laravel-mix');
 
 const destDir = 'public';
 
+mix.extend(
+    'zip',
+    new class {
+        register(src, dest, file) {
+            this.src = src;
+            this.dest = dest;
+            this.file = file;
+        }
+
+        dependencies() {
+            this.requiresReload = true;
+            return ['filemanager-webpack-plugin@2.*']
+        }
+
+        webpackRules() { }
+
+        webpackPlugins() {
+            const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+            return [
+                new FileManagerPlugin({
+                    onEnd: {
+                        archive: [
+                            { source: this.src, destination: `${this.dest}/${this.file}` }                            
+                        ]
+                    }
+                })
+            ]
+        }
+
+        // ...
+    }()
+);
+
 mix.sass('src/sass/site.scss', `${destDir}/css`)
+    .copy('src/helpers', `${destDir}/helpers`)
     .copy('src/layouts', `${destDir}/layouts`)
     .copy('src/partials', `${destDir}/partials`)
-    .browserSync('terrafarm-ui.local');
+    .zip('public', 'build', 'bundle.zip');
